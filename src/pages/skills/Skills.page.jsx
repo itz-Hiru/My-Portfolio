c41656949a8091fda05a4df278cf713f806607a5
itz-Hiru <hirumithagithub@gmail.com>
2025-04-05 10:59:58 +0530

    Created skills page

--- src/pages/skills/Skills.page.jsx
+++ src/pages/skills/Skills.page.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import Frontend from "../../components/skills/Frontend.component";
+import Backend from "../../components/skills/Backend.component";
+import "./Skills.page.css";
 
 const Skills = () => {
   return (
-    <div>Skills.page</div>
-  )
-}
+    <section className="skills section" id="skills">
+      <h2 className="section__title">Skills</h2>
+      <span className="section__subtitle">My technical level</span>
+      <div className="skills__container container grid">
+        <Frontend />
+        <Backend />
+      </div>
+    </section>
+  );
+};
 
-export default Skills
+export default Skills;
