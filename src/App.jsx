1b7b922588c709fae89028b4d473f4125ddd84fa
itz-Hiru <hirumithagithub@gmail.com>
2025-04-05 12:14:04 +0530

    Created scrollup component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header.component";
 import Home from "./pages/home/Home.page";
 import About from "./pages/about/About.page";
 import Skills from "./pages/skills/Skills.page";
 import Services from "./pages/services/Services.page";
 import Contact from "./pages/contact/Contact.page";
+import Scrollup from "./components/scrollup/Scrollup.component";
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
         <Services />
         <Contact />
         </main>
+        <Scrollup />
     </>
   );
 }
